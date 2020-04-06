ef897e1613752fd3773c62e6924c69f48e616d25
raulbethencourt <raul.bethencourt.pro@gmail.com>
2020-04-06 23:41:50 +0200

    morpion game

--- js/main.js
+++ js/main.js
@@ -4,15 +4,22 @@ $(document).ready(function () {
 
         if (count % 2) {
             $(this).append(player2);
+            $(this).css({
+                "background-color": "red",
+                "border-color": "red"
+            });
         } else {
             $(this).append(player1);
+            $(this).css({
+                "background-color": "black",
+                "color": "red"
+            });
         };
+        
         count++;
-
     });
 
-    const player1 = "X";
-    const player2 = "O";
+    const player1 = '<i class="fas fa-expand-arrows-alt"></i>';
+    const player2 = '<i class="fas fa-circle-notch"></i>';
     let count = 0;
-
 });
