1caf6f503a0df9ad5ab31fafc8d0e7ede5722e28
raulbethencourt <raul.bethencourt.pro@gmail.com>
2020-04-06 21:47:42 +0200

    js

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # morpion
 Morpion game. Create with javaScript.
+
+Here we have one of the exercises that i did during my web-developpment academic training.
--- js/main.js
+++ js/main.js
@@ -1,9 +1,18 @@
 $(document).ready(function () {
-    const player1 = "X"
-    const player2 = "O"
-    
-    $('.case').on("click", function () {
 
-    })
+    $('.case').click(function setPlayers() {
 
-})
+        if (count % 2) {
+            $(this).append(player2);
+        } else {
+            $(this).append(player1);
+        };
+        count++;
+
+    });
+
+    const player1 = "X";
+    const player2 = "O";
+    let count = 0;
+
+});
