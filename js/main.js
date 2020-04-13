69cbcc4ae7824f386ed024315091c666d8e8abd7
raulbethencourt <raul.bethencourt.pro@gmail.com>
2020-04-13 21:21:53 +0200

    javaScript

--- js/main.js
+++ js/main.js
@@ -1,11 +1,15 @@
 $(document).ready(function () {
 
-    $('.case').click(function setPlayers() {
+    $("#player1").css("text-decoration", "underline");
+    
+    $('.case').click(function () {
 
         if (count % 2) {
 
+            $("#player2").css("text-decoration", "underline");
+            $("#player1").css("text-decoration", "none");
+            
             $(this).append(player1);
-            console.log($(this).attr("id") + $(this).attr("coords"));
             $(this).css({
                 "background-color": "red",
                 "border-color": "red"
@@ -21,12 +25,6 @@ $(document).ready(function () {
                 v1_h3 += 1
             }
 
-            if (v1_h1 === 3 || v1_h2 === 3 || v1_h3 === 3) {
-                v1 = true;
-                alert("PLAYER 1 WINS");
-                location.reload();
-            }
-
             if (+$(this).attr("coords") == 1) {
                 v1_v1 += 1
             }
@@ -37,30 +35,42 @@ $(document).ready(function () {
                 v1_v3 += 1
             }
 
-            if (v1_v1 === 3 || v1_v2 === 3 || v1_v3 === 3) {
-                v1 = true;
-                alert("PLAYER 1 WINS");
-                location.reload();
-            }
-
             if (+$(this).attr("coords") == 2 && +$(this).attr("id") == 2) {
-                var v1_d = 1;
+                v1_d_c = 1;
             }
 
-            if (v1_d === 1 && 
-                ((v1_h1 === 1 && v1_v1 === 1) || (v1_h1 === 1 && v1_v3 === 1)  || 
-                (v1_h1 === 2 && v1_v1 === 2) || (v1_h1 === 2 && v1_v3 === 2 )) && 
-                ((v1_h3 === 1 && v1_v1 === 1) || (v1_h3 === 1 && v1_v3 === 1) || 
-                (v1_h3 === 2 && v1_v1 === 2) || (v1_h3 === 2 && v1_v3 === 2))) {
+            if (+$(this).attr("coords") == 1 && +$(this).attr("id") == 1) {
+                v1_d_r += 1;
+            }
+
+            if (+$(this).attr("coords") == 3 && +$(this).attr("id") == 3) {
+                v1_d_r += 1;
+            }
+
+            if (+$(this).attr("coords") == 3 && +$(this).attr("id") == 1) {
+                v1_d_l += 1;
+            }
+
+            if (+$(this).attr("coords") == 1 && +$(this).attr("id") == 3) {
+                v1_d_l += 1;
+            }
+
+            if ((v1_d_c === 1 && v1_d_r === 2) ||
+                (v1_d_c === 1 && v1_d_l === 2) ||
+                (v1_v1 === 3 || v1_v2 === 3 || v1_v3 === 3) ||
+                (v1_h1 === 3 || v1_h2 === 3 || v1_h3 === 3)) {
                 v1 = true;
-                alert("PLAYER 1 WINS");
-                location.reload();
+                $("#table").hide();
+                $("#player1").hide();
+                $("#player2").hide();
+                $("#wrapper").html('<div class="victory"><h1>PLAYER 1 WINS</h1><button type="button" onclick="location.reload()" id="continue">CONTINUE</button></div>');
             }
-
         } else {
 
+            $("#player1").css("text-decoration", "underline");
+            $("#player2").css("text-decoration", "none");
+            
             $(this).append(player2);
-            console.log($(this).attr("id") + $(this).attr("coords"));
             $(this).css({
                 "background-color": "black",
                 "color": "red"
@@ -76,12 +86,6 @@ $(document).ready(function () {
                 v2_h3 += 1
             }
 
-            if (v2_h1 === 3 || v2_h2 === 3 || v2_h3 === 3) {
-                v2 = true;
-                alert("PLAYER 2 WINS");
-                location.reload();
-            }
-
             if (+$(this).attr("coords") == 1) {
                 v2_v1 += 1
             }
@@ -92,22 +96,46 @@ $(document).ready(function () {
                 v2_v3 += 1
             }
 
-            if (v2_v1 === 3 || v2_v2 === 3 || v2_v3 === 3) {
-                v2 = true;
-                alert("PLAYER 2 WINS");
-                location.reload();
+            if (+$(this).attr("coords") == 2 && +$(this).attr("id") == 2) {
+                v2_d_c = 1;
             }
 
+            if (+$(this).attr("coords") == 1 && +$(this).attr("id") == 1) {
+                v2_d_r += 1;
+            }
+
+            if (+$(this).attr("coords") == 3 && +$(this).attr("id") == 3) {
+                v2_d_r += 1;
+            }
+
+            if (+$(this).attr("coords") == 3 && +$(this).attr("id") == 1) {
+                v2_d_l += 1;
+            }
+
+            if (+$(this).attr("coords") == 1 && +$(this).attr("id") == 3) {
+                v2_d_l += 1;
+            }
+
+            if ((v2_d_c === 1 && v2_d_r === 2) ||
+                (v2_d_c === 1 && v2_d_l === 2) ||
+                (v2_v1 === 3 || v2_v2 === 3 || v2_v3 === 3) ||
+                (v2_h1 === 3 || v2_h2 === 3 || v2_h3 === 3)) {
+                v2 = true;
+                $("#table").hide();
+                $("#player1").hide();
+                $("#player2").hide();
+                $("#wrapper").html('<div class="victory"><h1>PLAYER 2 WINS</h1><button type="button" onclick="location.reload()" id="continue">CONTINUE</button></div>');
+            };
         };
 
         count--;
         if (count == 0 && v1 === false && v2 === false) {
-            document.getElementById("v").innerHTML = "MATCH NULL";
-            location.reload();
+            $("#table").hide();
+            $("#player1").hide();
+            $("#player2").hide();
+            $("#wrapper").html('<div class="victory"><h1>MATCH NULL</h1><button type="button" onclick="location.reload()" id="continue">CONTINUE</button></div>');
         }
     });
-
-
 
     const player1 = '<i class="fas fa-expand-arrows-alt"></i>';
     const player2 = '<i class="fas fa-circle-notch"></i>';
@@ -120,12 +148,16 @@ $(document).ready(function () {
     let v2_h1 = 0;
     let v2_h2 = 0;
     let v2_h3 = 0;
-    var v1_v1 = 0;
-    var v1_v2 = 0;
-    var v1_v3 = 0;
-    var v2_v1 = 0;
-    var v2_v2 = 0;
-    var v2_v3 = 0;
-    var v1_d = 0;
-    var v2_d = 0;
+    let v1_v1 = 0;
+    let v1_v2 = 0;
+    let v1_v3 = 0;
+    let v2_v1 = 0;
+    let v2_v2 = 0;
+    let v2_v3 = 0;
+    let v1_d_c = 0;
+    let v1_d_r = 0;
+    let v1_d_l = 0;
+    let v2_d_c = 0;
+    let v2_d_r = 0;
+    let v2_d_l = 0;
 });
